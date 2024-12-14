6e2c37cc1b3963f2b4a00210e88a54ff6ec5bc58
samuel.casanova <samuel.casanova@immfly.com>
2024-12-14 20:50:06 +0100

    refactor the unit test to create a DSL with given-when-then syntax in all tests

--- test/unit/video.unit.test.ts
+++ test/unit/video.unit.test.ts
@@ -18,22 +18,45 @@ const eventBus = {
 }
 
 describe('Video', () => {
-  it('should create a video from primitives', async () => {
-    const createVideoCommand = new CreateVideoCommand(videoIdValue, videoTitleValue)
-
-    const handler = new CreateVideoCommandHandler(repository, eventBus)
-    await handler.handle(createVideoCommand)
-
-    expect(repository.save).toHaveBeenCalled()
-    expect(eventBus.publishAll.mock.calls[0][0]).toHaveLength(1)
-    expect(eventBus.publishAll.mock.calls[0][0][0]).toBeInstanceOf(VideoCreatedEvent)
+  it('should create a video', async () => {
+    const createVideoCommand = givenaUserWantsToCreateaVideo()
+    await whenTheVideoIsCreated(createVideoCommand)
+    thenItsSavedInTheRepositoryAndAnEventIsPublished()
   })
+
   it('should retrieve all stored videos', async () => {
-    (repository.searchAll).mockResolvedValue([Video.fromPrimitives(videoIdValue, videoTitleValue)])
-
-    const handler = new SearchAllVideosQueryHandler(repository)
-    const videos = await handler.handle()
-
-    expect(videos).toHaveLength(1)
+    givenaVideoIsInTheRepository()
+    const videos = await whenaUserSearchsForAllVideos()
+    thenTheyFindTheVideo(videos)
   })
 })
+
+function thenTheyFindTheVideo (videos: Video[]): void {
+  expect(repository.searchAll).toHaveBeenCalled()
+  expect(videos).toHaveLength(1)
+}
+
+async function whenaUserSearchsForAllVideos (): Promise<Video[]> {
+  const handler = new SearchAllVideosQueryHandler(repository)
+  const videos = await handler.handle()
+  return videos
+}
+
+function givenaVideoIsInTheRepository (): void {
+  (repository.searchAll).mockResolvedValue([Video.fromPrimitives(videoIdValue, videoTitleValue)])
+}
+
+function thenItsSavedInTheRepositoryAndAnEventIsPublished (): void {
+  expect(repository.save).toHaveBeenCalled()
+  expect(eventBus.publishAll.mock.calls[0][0]).toHaveLength(1)
+  expect(eventBus.publishAll.mock.calls[0][0][0]).toBeInstanceOf(VideoCreatedEvent)
+}
+
+async function whenTheVideoIsCreated (createVideoCommand: CreateVideoCommand): Promise<void> {
+  const handler = new CreateVideoCommandHandler(repository, eventBus)
+  await handler.handle(createVideoCommand)
+}
+
+function givenaUserWantsToCreateaVideo (): CreateVideoCommand {
+  return new CreateVideoCommand(videoIdValue, videoTitleValue)
+}
