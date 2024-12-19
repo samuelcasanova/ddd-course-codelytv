6210d8df40a2cc6eca4f79f5edfd3d7d6b465e4b
samuel.casanova <samuel.casanova@immfly.com>
2024-12-19 07:50:06 +0100

    create independent controllers for each route handler

--- src/video/infrastructure/videoRouter.ts
+++ src/video/infrastructure/videoRouter.ts
@@ -1,30 +1,9 @@
-import express, { type Request, type Response } from 'express'
-import { SQLiteVideoRepository } from './SQLiteVideoRepository'
-import { SearchAllVideosQueryHandler } from '../application/SearchAllVideosQueryHandler'
-import { CreateVideoCommand } from '../application/CreateVideoCommand'
-import { CreateVideoCommandHandler } from '../application/CreateVideoCommandHandler'
-import { eventBus } from '../../shared/infrastructure/eventEmitterEventBus'
+import express from 'express'
+import { getVideoController } from './getVideoController'
+import { postVideoController } from './postVideoController'
 
 export const videoRouter = express.Router()
 
-videoRouter.get(
-  '/',
-  async (req: Request, res: Response) => {
-    const repository = await SQLiteVideoRepository.create()
-    const searchAllVideosQueryHandler = new SearchAllVideosQueryHandler(repository)
+videoRouter.get('/', getVideoController)
 
-    const videos = await searchAllVideosQueryHandler.handle()
-
-    res.send(videos.map(video => video.toPrimitives()))
-  }
-)
-
-videoRouter.post('/', async (req: Request, res: Response) => {
-  const repository = await SQLiteVideoRepository.create()
-  const createVideoCommand = new CreateVideoCommand(req.body.id, req.body.title)
-  const createVideoCommandHandler = new CreateVideoCommandHandler(repository, eventBus)
-
-  await createVideoCommandHandler.handle(createVideoCommand)
-
-  res.status(201).send()
-})
+videoRouter.post('/', postVideoController)
